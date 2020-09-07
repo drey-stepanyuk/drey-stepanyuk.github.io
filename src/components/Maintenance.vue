a58fd5c5ce1da690b4695a664ee9a85f29470e4d
Drey Stepanyuk <drey.stepanyuk@gmail.com>
2020-09-06 19:24:13 -0600

    add deploy script

---
+++ src/components/Maintenance.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Construction">
+    <h1>Website is currently under maintenance </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
